7ed9b8f46d74cb5f8c6849bc4bb98885cdffb2fd
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2023-12-07 11:15:40 +0000

    Fix missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./stories/navigation/Flipper";
 export * from "./stories/navigation/Pagination";
 export * from "./stories/navigation/Navbar";
+export * from "./stories/navigation/Breadcrumbs";
 
 export * from "./stories/visualisation/InfoGroup";
 export * from "./stories/visualisation/Image";
@@ -13,6 +14,7 @@ export * from "./stories/plots/Bar";
 
 export * from "./stories/control/User";
 export * from "./stories/control/DebouncedInput";
+export * from "./stories/control/DarkModeButton";
 
 export * from "./styles/theme";
 export * from "./styles/colours";
